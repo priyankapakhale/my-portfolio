49932379428075ca7544ab15429ebda65ce6e85d
Priyanka Pakhale <priyanka@technogise.com>
2025-07-05 02:59:54 +0530

    Initialized repo and added config files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen bg-red-500 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">Test Background 🔴</h1>
+    </div>
+  );
+}
+
+export default App;
