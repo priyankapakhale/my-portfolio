4c3fc0e69c9e79f6c3ff94b26cf0a95142aebd86
Priyanka Pakhale <priyanka.pakhale54@gmail.com>
2025-07-05 03:53:13 +0530

    created basic layout with placeholder sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import "./App.css";
+import Portfolio from "./components/Portfolio";
 
 function App() {
-  return (
-    <div className="h-screen bg-red-500 flex items-center justify-center">
-      <h1 className="text-white text-4xl font-bold">Test Background 🔴</h1>
-    </div>
-  );
+  return <Portfolio />;
 }
 
 export default App;
